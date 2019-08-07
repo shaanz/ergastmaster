3efea0dcd972343485e8a30cea7d3cc913480f97
Bien <bien@MacBook-Air.local>
2019-08-07 15:03:50 +0700

    adding_script

---
+++ initdb/clean_mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "deleting mysql service on k8s cluster"
+kubectl delete service mysql
+kubectl get svc
+
+echo "deleting mysql deployment"
+kubectl delete deployment mysql
+kubectl get deploy
+
+echo "deleting mysql-pv-claim"
+kubectl delete pvc mysql-pv-claim
+kubectl get pvc
+
+echo "deleting mysql-pv-volume"
+kubectl delete pv mysql-pv-volume
+kubectl get pv
+
---
+++ initdb/create_mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "create mysql-pv-volume"
+kubectl create -f mysql-pv.yaml
+kubectl get pv
+
+echo "create mysql-pv-claim"
+kubectl create -f mysql-pvc.yaml
+kubectl get pvc
+
+echo "create mysql deployment"
+kubectl create -f mysql-deployment.yaml
+kubectl get deploy
+
+echo "create mysql service"
+kubectl create -f mysql-service.yaml
+
