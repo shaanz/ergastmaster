f2e607e6d1590b56fb63a5a5819ae1ac579a7715
Jenkins Automation Server <jenkins@ip-172-31-4-66.ap-east-1.compute.internal>
2019-08-09 20:53:36 +0700

    fix

--- test/testapif1.sh
+++ test/testapif1.sh
@@ -3,10 +3,12 @@ curl -i -s http://ergastapp.bienlab.com/api/f1.json > /tmp/testapif1.txt
 result=`grep 200 /tmp/testapif1.txt | wc -l`
 if [ $result -gt 0 ]
 then
-exit 0
-else
-exit 1
-fi
 cat /tmp/testapif1.txt
 rm  /tmp/testapif1.txt
+exit 0
+else
+cat /tmp/testapif1.txt
+rm  /tmp/testapif1.txt
+exit 1
+fi
 
