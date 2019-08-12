b802838ee892461a529fa8b5dd62fc7b62498758
Bien <bien@bienlab.com>
2019-08-13 02:22:47 +0700

    fix test code

--- test/testdbconnect.sh
+++ test/testdbconnect.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#wait for the app start
+fn=0
+while [ $fn -ne 1 ]
+do
+result=`kubectl get pod | grep ergastapp | grep Running | wc -l`
+  if [ $result -gt 1 ]
+  then
+   fn=1
+  fi
+done
+
 result=`curl -i -s http://ergastapp.bienlab.com/api/f1.json | grep "Unable to select database" | wc -l`
 if [ $result -gt 0 ]
 then
